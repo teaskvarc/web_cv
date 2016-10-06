fae3594ad218b0bcf92b328969306b8f46a18134
teaskv <teaskvarc@gmail.com>
2016-10-06 22:11:29 +0200

    added style, handled back to top

--- js/main.js
+++ js/main.js
@@ -12,3 +12,48 @@ $(document).ready(function () {
         $('.side-menu-transparent').fadeIn(300);
     });
 });
+
+
+$(document).ready(function () {
+   $(window).scroll(function () {
+       if($(this).scrollTop() > 500){
+           $('#back-to-top').fadeIn();
+       }else {
+           $('#back-to-top').fadeOut();
+       }
+   });
+
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+
+});
+
+
+
+// Preloader
+
+$(window).load(function () {
+    $('#loading-center').click(function () {
+        $('#loading').fadeOut(500);
+    });
+});
+
+$(window).load(function () {
+   $('#loading').fadeOut(500);
+});
+
+$(window).load(function () {
+   //$('#loading').delay(2000).fadeOut(500);
+    $('#loading-center').click(function () {
+        $('#loading').fadeOut(500);
+    });
+});
+
