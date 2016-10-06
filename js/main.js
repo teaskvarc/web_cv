f867ead6e517eeb276c9fcddf533e57125051200
teaskv <teaskvarc@gmail.com>
2016-10-06 17:01:41 +0200

    added jQuery function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-/**
- * Created by Tea on 5.10.2016.
- */
+// SIDE MENU SCRIPTS
+
+$(document).ready(function () {
+   $('.menu-close-icon, .side-menu-transparent').click(function (e) {
+      e.preventDefault();
+       $('#side-menu').animate({left: '-250px', top:'-1000px'});
+       $('.side-menu-transparent').fadeOut(300);
+   });
+    $('.menu-open-icon').click(function (e) {
+        e.preventDefault();
+        $('#side-menu').animate({left:'0px', top:'0px'});
+        $('.side-menu-transparent').fadeIn(300);
+    });
+});
