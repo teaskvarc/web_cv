ac829f9131364afc430eca69c9b0fc838c69e2be
teaskv <teaskvarc@gmail.com>
2016-10-07 16:39:34 +0200

    finished responsive

--- js/main.js
+++ js/main.js
@@ -37,7 +37,6 @@ $(document).ready(function () {
 });
 
 
-
 // Preloader
 
 $(window).load(function () {
@@ -57,3 +56,19 @@ $(window).load(function () {
     });
 });
 
+
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
