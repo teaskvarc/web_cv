19bf15fb0c1545f28692101303c784b9a476dab3
teaskv <teaskvarc@gmail.com>
2016-10-18 18:03:55 +0200

    minified js,css and images

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var rename      = require('gulp-rename');
 var coffee      = require('gulp-coffee');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
+var gulpIf      = require('gulp-if');
 var imagemin    = require('gulp-imagemin');
 var sourcemaps  = require('gulp-sourcemaps');
 var del         = require('del');
 var useref      = require('gulp-useref');
+var cssnano     = require('gulp-cssnano');
+
 
 gulp.task('test', function () {
 
@@ -26,6 +29,13 @@ gulp.task('useref', function () {
     return gulp.src('**/*.html')
         .pipe(useref())
         .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function () {
+    return gulp.src('images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+
 });
 
 
